a41e5ea668af39dce6b94e21ffa979d3fdb8b367
Brandon Tsai <bjt888@gmail.com>
2022-09-12 17:27:39 -0700

    implements fetched setlist data from pokemon api

--- pages/set-list.js
+++ pages/set-list.js
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import NavBar from "../comps/NavBar";
-import Footer from "../comps/Footer";
-import setDummyData from "../set-dummy-data";
 import Set from "../comps/Set";
-import axios from 'axios';
 
-const SetList = () => {
-  const [currentSetList, setCurrentSetList] = useState([]);
+export async function getServerSideProps() {
+  const res = await fetch('https://api.pokemontcg.io/v2/sets?orderBy=-releaseDate');
+  const data = await res.json();
 
-  useEffect(() => {
-    axios.get('https://api.pokemontcg.io/v2/sets?orderBy=-releaseDate')
-      .then((response) => {
-        console.log(response.data);
-      })
-  }, [])
+  return {
+    props: {
+      setList: data
+    }
+  }
+}
+
+const SetList = ({ setList }) => {
+  console.log(setList)
 
   return ( 
     <div>
       <h1>Set List</h1>
       <div>
-        {setDummyData.data.map((setInfo) => 
+        {setList.data.map((setInfo) => 
           <Set
             setInfo={setInfo}
             key={setInfo.id}
